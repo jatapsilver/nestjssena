0a0e483c8b5b8b22fdb28774e25e4e3874e7c56e
jatapsilver <jatapsilver92@hotmail.com>
2025-09-18 10:49:57 -0500

    class18/09/2025

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('Se envio la respuesta del getHello');
     return 'Bienvenidos a Sena mujeres digitales';
   }
 }
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { loggerGlobal } from './middlewares/loggerGlobal';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(loggerGlobal);
   await app.listen(3002);
+  console.log('Servidor corriendo en el puerto 3002');
 }
 bootstrap();
