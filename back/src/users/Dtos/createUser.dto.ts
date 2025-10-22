a6345d11f58529a201229786a3cda88bbcec9f33
jatapsilver <jatapsilver92@hotmail.com>
2025-10-21 23:46:59 -0500

    clase 22 21 de octubre

--- back/src/users/Dtos/createUser.dto.ts
+++ back/src/users/Dtos/createUser.dto.ts
@@ -8,27 +8,21 @@ import {
 } from 'class-validator';
 
 export class CreatedUserDto {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsNotEmpty({ message: 'El nombre es requerido' })
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsString({
     message: 'El nombre debe ser una cadena de caracteres',
   })
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @Matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/, {
     message: 'El nombre solo puede contener letras y espacios',
   })
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @MinLength(3, {
     message: 'El nombre debe tener minimo 3 caracteres',
   })
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @MaxLength(25, {
     message: 'El nombre no puede contener mas de 25 caracteres',
   })
   name: string;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsNotEmpty({
     message: 'El apellido es requerido',
   })
@@ -38,18 +32,16 @@ export class CreatedUserDto {
   @Matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/, {
     message: 'El apellido solo puede contener letras y espacios',
   })
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @MinLength(3, {
     message: 'El apellido debe tener minimo 3 caracteres',
   })
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @MaxLength(25, {
     message: 'El apellido no puede contener mas de 25 caracteres',
   })
   lastName: string;
+
   address: string;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsEmail()
   email: string;
   phoneNumber: number;
