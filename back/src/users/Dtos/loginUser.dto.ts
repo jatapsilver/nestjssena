e696f72c76c88e6a9ea3317da7801bdeb6d144cf
jatapsilver <jatapsilver92@hotmail.com>
2025-11-04 22:43:22 -0500

    update

--- back/src/users/Dtos/loginUser.dto.ts
+++ back/src/users/Dtos/loginUser.dto.ts
@@ -1,4 +1,4 @@
-import { PickType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/swagger';
 import { CreatedUserDto } from './createUser.dto';
 
 export class LoginUserDto extends PickType(CreatedUserDto, [
--- back/src/users/Dtos/updateUser.dto.ts
+++ back/src/users/Dtos/updateUser.dto.ts
@@ -1,8 +1,14 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreatedUserDto } from './createUser.dto';
 import { IsNotEmpty, IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto extends PartialType(CreatedUserDto) {
+  @ApiProperty({
+    description: 'UUID del usuario a actualizar',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+    format: 'uuid',
+  })
   @IsNotEmpty({ message: 'El id del usuario es obligatorio' })
   @IsUUID('4', { message: 'El id del usuario debe tener un formato UUID' })
   uuid: string;
